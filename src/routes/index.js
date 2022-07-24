a93dd1a27f399d1259de465c3c69a3ce2327b9cb
calmighted <manishh@tutanota.com>
2022-07-24 10:22:04 +0530

    all weather message

--- src/routes/index.js
+++ src/routes/index.js
@@ -40,20 +40,23 @@ router.post('/weather', async (req, res) => {
 
 router.post('/sms', async (req, res) => {
   try {
-
     let lat = req.body.Latitude;
     let lon = req.body.Longitude;
 
-    let weather = await getWeather(lat, lon);
+    const weatherMessage = await getWeather(lat, lon);
 
-    let sampleText = makeMessage(weather);
+    // const message = new MessagingResponse().message(weatherMessage);
+    // res.set('Content-Type', 'text/xml');
+    // res.send(message.toString()).status(200);
+    console.log(weatherMessage);
 
 
-    const message = new MessagingResponse().message(sampleText);
+
+
+    // send message from twilio api
+    const message = new MessagingResponse().message(weatherMessage);
     res.set('Content-Type', 'text/xml');
     res.send(message.toString()).status(200);
-
-
   } catch (error) {
     console.log(error);
   }
@@ -64,42 +67,40 @@ const getWeather = async (latitude, longitude) => {
 
   const result = await makeRequest('GET', url);
 
-  return result;
+
+  console.log(result);
+
+  const weatherMessage = makeMessage(result);
+
+  return weatherMessage;
 };
 
+const makeMessage = (w) => {
+  // console.log(w.body.list[0]);
 
-const makeMessage = (weather) => {
-  console.log(weather.body.list[0]);
+  currentTemp = w.body.list[0].main.temp;
+  currentWeather = w.body.list[0].weather[0].description;
+  currentPop = w.body.list[0].pop;
 
-  currentTemp = weather.body.list[0].main.temp;
-  currentWeather = weather.body.list[0].weather[0].description;
-  currentPop = weather.body.list[0].pop;
- 
-  let message = `Current Weather\n\n${currentTemp}°C degrees - ${currentWeather} \nProbability of rain is ${(currentPop) * 100}%\n____________________________________________\n`;
+  let message = `Current Weather\n\n${currentTemp}°C degrees - ${currentWeather} \nProbability of rain is ${
+    currentPop * 100
+  }%\n____________________________________________\n`;
+
+  const listLength = w.body.list.length;
+
+  for (let i = 1; i < listLength; i++) {
+    let day = w.body.list[i].dt_txt.split(' ')[0];
+    let time = w.body.list[i].dt_txt.split(' ')[1];
+    let temp = w.body.list[i].main.temp;
+    let weather = w.body.list[i].weather[0].description;
+    let pop = w.body.list[i].pop;
+
+    message += `${day} ${time}\n\n${temp}°C degrees - ${weather} \nProbability of rain is ${
+      pop * 100
+    }%\n____________________________________________\n\n`;
+  }
 
   return message;
-}
-
-const MessageConsole = () => {
-  getWeather(18.67, 73.71).then((weather) => {
-    console.log(makeMessage(weather));
-
-    // send message to user
-    // const message = new MessagingResponse().message(makeMessage(weather));
-    // res.set('Content-Type', 'text/xml');
-    // res.send(message.toString()).status(200);
-
-
-  }
-  ).catch((error) => {
-    console.log(error);
-  }
-  );
-}
-
-// MessageConsole();
-
-
-
+};
 
 module.exports = router;
